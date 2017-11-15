7427ce43f50496ffb16a89337be83fbcce076b64
Endri Azizi <endri.azizi@gmail.com>
2017-11-15 14:49:27 +0100

    added template handlebars engine

--- README.md
+++ README.md
@@ -31,3 +31,62 @@ RUN app.js
     node app.js 
 or
     node app
+
+nodemon will watch the files in the directory in which nodemon was started, and if any files change, nodemon will automatically restart your node application.
+    npm install -g nodemon
+
+where our global modules are installed locally:
+    npm root -g
+
+http://handlebarsjs.com/
+https://github.com/ericf/express-handlebars
+
+    npm install --save express-handlebars
+
+  `-- express-handlebars@3.0.0
+  +-- glob@6.0.4
+  | +-- inflight@1.0.6
+  | | `-- wrappy@1.0.2
+  | +-- minimatch@3.0.4
+  | | `-- brace-expansion@1.1.8
+  | |   +-- balanced-match@1.0.0
+  | |   `-- concat-map@0.0.1
+  | +-- once@1.4.0
+  | `-- path-is-absolute@1.0.1
+  +-- graceful-fs@4.1.11
+  +-- handlebars@4.0.11
+  | +-- async@1.5.2
+  | +-- optimist@0.6.1
+  | | +-- minimist@0.0.10
+  | | `-- wordwrap@0.0.3
+  | +-- source-map@0.4.4
+  | | `-- amdefine@1.0.1
+  | `-- uglify-js@2.8.29
+  |   +-- source-map@0.5.7
+  |   +-- uglify-to-browserify@1.0.2
+  |   `-- yargs@3.10.0
+  |     +-- camelcase@1.2.1
+  |     +-- cliui@2.1.0
+  |     | +-- center-align@0.1.3
+  |     | | +-- align-text@0.1.4
+  |     | | | +-- kind-of@3.2.2
+  |     | | | | `-- is-buffer@1.1.6
+  |     | | | +-- longest@1.0.1
+  |     | | | `-- repeat-string@1.6.1
+  |     | | `-- lazy-cache@1.0.4
+  |     | +-- right-align@0.1.3
+  |     | `-- wordwrap@0.0.2
+  |     +-- decamelize@1.2.0
+  |     `-- window-size@0.1.0
+  +-- object.assign@4.0.4
+  | +-- define-properties@1.1.2
+  | | `-- foreach@2.0.5
+  | +-- function-bind@1.1.1
+  | `-- object-keys@1.0.11
+  `-- promise@7.3.1
+    `-- asap@2.0.6
+
+
+TIPS: this come with Emett, Visual Code comes by default with it, 
+to others ide you need to install plugin for it,
+h1 and tab to autocomplet every tag you need
--- app.js
+++ app.js
@@ -4,18 +4,60 @@ instead of var, fat arrow, promises are Es6 sintax
 */
 const express = require('express')
 const app = express()
- 
+const exphbs  = require('express-handlebars')
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+/*How middleware works 
+http://expressjs.com/it/guide/using-middleware.html
+*/
+/* app.use(function(req, res, next){
+    //console.log('Date ' + Date.now());
+    req.name = 'Endri Azizi';
+    next();
+}); */
+
+/*
+we have access to that req [request] obkect we added something to it*/ 
+app.use((req, res, next)=>{
+    //console.log('Date ' + Date.now());
+    req.name = 'Endri Azizi';
+    next();
+});
+
 //Index Route
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+/* app.get('/', function (req, res) {
+    res.send('Hello World');
+}); */
+
+//Handlebar use render for send to the browser
+app.get('/',(req, res) => {
+    //inside we define the index handlebars page for randering at that  route
+    //passing dynamic data to our view
+    const title = 'Welcome Back';
+    res.render('index', {
+        title: title
+    });
+    
+
+});
 
 app.get('/test', (req, res) => {
     //send something to the browser
-    res.send('TEST')
-})
+    console.log(req.name);
+    res.send('TEST');
+});
 
-  
+app.get('/about', (req, res) => {
+    //send something to the browser
+    res.send('ABOUT');
+});
+
+
 const port = 5000
 
 //listen method
@@ -23,11 +65,14 @@ const port = 5000
 /* app.listen(port, function(){
 
 }); */
-//ARROW FUNCTION
-//https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/template_strings
-//http://exploringjs.com/es6/ch_template-literals.html
-app.listen(port, ()=>{
+
+/*
+ARROW FUNCTION
+https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/template_strings
+http://exploringjs.com/es6/ch_template-literals.html
+*/
+app.listen(port, () => {
     //template string or template literal using back ticks
     console.log(`Server started at ${port}`);
-    console.log("Server started at "+ port);
+    console.log("Server started at " + port);
 });
